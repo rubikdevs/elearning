2c56348f6d7c936358dbebf6a6b06b4aeb3850e3
Brychan Toda <brychan.toda@gmail.com>
2013-11-13 02:19:57 -0300

    timer

--- themes/esthetics/views/elearning/page.php
+++ themes/esthetics/views/elearning/page.php
@@ -26,5 +26,32 @@
 		if ($status)
 			echo CHtml::link('Next',array('elearning/page', 'module'=>$page->module_code, 'page_number'=>$page->page_number+1),array('class'=>'float_r button'));
 		else 
-			echo CHtml::link('Question',array('elearning/qa', 'page_code'=>$page->page_code),array('class'=>'float_r button'));
+		{	echo CHtml::link('Question',array('elearning/qa', 'page_code'=>$page->page_code),array('class'=>'float_r button','id'=>'page-timer'));
+			?>
+
+			<script type="text/javascript" >
+				var downloadButton = document.getElementById("page-timer");
+				var counter = 5;
+				var newElement = document.createElement("p");
+				newElement.innerHTML = "You can continue in 90 seconds.";
+				var id;
+
+				downloadButton.parentNode.replaceChild(newElement, downloadButton);
+
+				id = setInterval(function() {
+				    counter--;
+				    if(counter < 0) {
+				        newElement.parentNode.replaceChild(downloadButton, newElement);
+				        clearInterval(id);
+				    } else {
+				        newElement.innerHTML = "You can continue in " + counter.toString() + " seconds.";
+				    }
+				}, 1000);
+			</script>
+			<?
+			
+		}
 	?>
+<script type="text/javascript" >
+
+</script>
